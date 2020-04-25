d68bb6039e2d753c7950cce080538f2f1621770c
Shahaf Mezin <smezin@gmail.com>
2020-04-25 12:27:03 +0300

    Initial Commit

---
+++ CheckersOnlineSwiftV2.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckersOnlineSwiftV2.1
+//
+//  Created by hyperactive on 25/04/2020.
+//  Copyright © 2020 hyperActive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
